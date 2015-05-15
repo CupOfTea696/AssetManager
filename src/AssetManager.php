9f8e896af6c88a8f4a449936d85152af6f854d15
CupOfTea696 <cupoftea696@gmail.com>
2015-05-15 14:31:32 +0200

    Release: v1.2.1

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -15,7 +15,7 @@ class AssetManager implements ProviderContract
      * @const string VERSION
      */
     const PACKAGE = 'CupOfTea/AssetManager';
-    const VERSION = '1.2.0';
+    const VERSION = '1.2.1';
     
     /**
      * Get an array with uncompressed and minified files for an asset.
