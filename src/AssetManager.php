83defc46e547981ccb607ef0b8ec38a49209671a
CupOfTea696 <cupoftea696@gmail.com>
2015-05-15 16:33:20 +0200

    Release: v1.2.2

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -15,7 +15,7 @@ class AssetManager implements ProviderContract
      * @const string VERSION
      */
     const PACKAGE = 'CupOfTea/AssetManager';
-    const VERSION = '1.2.1';
+    const VERSION = '1.2.2';
     
     /**
 	 * {@inheritdoc}
