db57107e070edc2a996b2b2cb5bd611c21502534
Sven Wittevrongel <cupoftea696@gmail.com>
2016-04-13 13:17:55 +0100

    Release: v1.3.1

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -15,7 +15,7 @@ class AssetManager implements ProviderContract
      * @const string VERSION
      */
     const PACKAGE = 'CupOfTea/AssetManager';
-    const VERSION = '1.3.0';
+    const VERSION = '1.3.1';
     
     protected $cfg;
     
