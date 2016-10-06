6b35347c3eec04c02f9633cfe0f53995c2cd278b
Sven Wittevrongel <cupoftea696@gmail.com>
2016-10-06 17:41:23 +0100

    Release: v1.6.3

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -21,7 +21,7 @@ class AssetManager implements ProviderContract
      *
      * @const string
      */
-    const VERSION = '1.6.2';
+    const VERSION = '1.6.3';
     
     /**
      * Asset Manager configuration.
